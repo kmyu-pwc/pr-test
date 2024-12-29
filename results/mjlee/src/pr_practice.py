f82c5a561f9b638094fc23257b8a8c656d8e9582
mjlee <myungjun.lee@pwc.com>
2024-12-29 23:58:49 +0900

    Modify name and email in pr_practice.py

--- results/mjlee/src/pr_practice.py
+++ results/mjlee/src/pr_practice.py
@@ -5,5 +5,5 @@
 # print("My name is Hong gil-dong")
 # print("Please contact me at gd.hong@email.com")
 
-print("My name is <NAME>")
-print("Please contact me at <EMAIL>")
+print("My name is myungjun.lee")
+print("Please contact me at myungjun.lee@pwc.com")
