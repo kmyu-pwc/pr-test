d6c1030368c572592fe643814bc8904f411c3898
chaen <with.ceseo@gmail.com>
2025-01-05 16:17:07 +0900

    Modify name and email in pr_practice.py

--- results/chaeeunseo/src/pr_practice.py
+++ results/chaeeunseo/src/pr_practice.py
@@ -5,5 +5,5 @@
 # print("My name is Hong gil-dong")
 # print("Please contact me at gd.hong@email.com")
 
-print("My name is <NAME>")
-print("Please contact me at <EMAIL>")
+print("My name is Seo chae-eun")
+print("Please contact me at chaeeun.seo@pwc.com")
